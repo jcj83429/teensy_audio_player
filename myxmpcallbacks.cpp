4b9460f16bd2d552f4155c64f462b95158b8202e
jcj83429 <jcj83429@gmail.com>
2021-01-17 10:44:40 -0800

    do SD reads into PSRAM in 2 steps to improve reliability
    For some reason, reading directly from SD card into PSRAM sometimes causes the SD read operations to fail.
    It seems that concurrent SD and PSRAM access is not reliable.
    To fix it, first read from SD to internal RAM then copy from internal RAM to PSRAM

--- myxmpcallbacks.cpp
+++ myxmpcallbacks.cpp
@@ -1,5 +1,11 @@
 #include <SdFat.h>
 
+// Do my_xmp_read in two steps:
+// first read from SD card to DMAMEM (malloc) then copy from DMAMEM to PSRAM
+// This slows down the reads but seems to improve the reliability of SD card reads
+// Maybe the SD card and PSRAM are interfering with each other because they're so close
+#define TWO_STEP_XMP_FILE_READ 1
+
 size_t my_xmp_read(void *user_data, void *buf, size_t size, size_t num){
 /*
   Serial.print(__FUNCTION__);
@@ -10,20 +16,57 @@ size_t my_xmp_read(void *user_data, void *buf, size_t size, size_t num){
   Serial.print(" ");
   Serial.println(num);
 */
+  
+#if TWO_STEP_XMP_FILE_READ
+  // All of libxmp's memory allocations are in the PSRAM,
+  // so there is plenty of malloc memory available
+  const int XMP_READ_BUF_SIZE = 128*1024;
+  char *xmp_read_buf = (char*) malloc(XMP_READ_BUF_SIZE);
+  if(!xmp_read_buf){
+    return 0;
+  }
+  size_t total_bytes_to_read = size * num;
+  size_t total_bytes_read = 0;
+  char *charBuf = (char*)buf;
   FsFile *file = (FsFile *)user_data;
-  int bytes_read = file->read(buf, size * num);
-  if(bytes_read != size * num){
+  while(total_bytes_read < total_bytes_to_read){
+    size_t bytes_to_read = min(total_bytes_to_read - total_bytes_read, sizeof(xmp_read_buf));
+    size_t bytes_read = file->read(xmp_read_buf, bytes_to_read);
+    if(bytes_read != bytes_to_read){
+      Serial.print("my_xmp_read: tried to read ");
+      Serial.print(bytes_to_read);
+      Serial.print(" bytes but got ");
+      Serial.print(bytes_read);
+      Serial.print(" bytes. read a total of ");
+      Serial.print(total_bytes_read);
+      Serial.print("/");
+      Serial.print(total_bytes_to_read);
+      Serial.println("bytes");
+      break;
+    }else{
+      memcpy(charBuf, xmp_read_buf, bytes_read);
+      arm_dcache_flush_delete(charBuf, bytes_read);
+      charBuf += bytes_read;
+      total_bytes_read += bytes_read;
+    }
+  }
+  free(xmp_read_buf);
+#else
+  FsFile *file = (FsFile *)user_data;
+  int total_bytes_read = file->read(buf, size * num);
+  if(total_bytes_read != size * num){
     Serial.print("my_xmp_read: tried to read ");
     Serial.print(size * num);
     Serial.print(" bytes but got ");
-    Serial.print(bytes_read);
+    Serial.print(total_bytes_read);
     Serial.println(" bytes. ");
   }
+#endif
   /*
   Serial.print("return ");
   Serial.println(bytes_read / size);
   */
-  return bytes_read / size;
+  return total_bytes_read / size;
 }
 
 int my_xmp_seek(void *user_data, long offset, int whence){
